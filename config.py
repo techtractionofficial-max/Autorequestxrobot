66094e35c431596ca85510c692942f75db49c30a
techtractionofficial-max <techtractionofficial@gmail.com>
2026-08-23 06:03:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,6 +17,6 @@ class Config:
 config = Config()
 
     # Add these below your existing variables in config.py
-    START_PHOTO_URL: str = "https://images.unsplash.com/photo-1618401471353-b98afee0b2eb?q=80&w=800&auto=format&fit=crop"
-    WELCOME_PHOTO_URL: str = "https://images.unsplash.com/photo-1556155092-490a1ba16284?q=80&w=800&auto=format&fit=crop"
+    START_PHOTO_URL: str = "https://t.me/photouploadhere/21"
+    WELCOME_PHOTO_URL: str = "https://t.me/photouploadhere/22"
 
